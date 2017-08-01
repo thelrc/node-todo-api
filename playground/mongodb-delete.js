053b0ed1c97d8c3914721befe0455973d13fc861
Luis Ramos <lr2471311@gmail.com>
2017-07-31 19:38:48 -0700

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,35 @@
+const {MongoClient, ObjectID} = require("mongodb"); 
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if(err) {
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+	//deleteMany
+	// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	//deleteOne
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	//findOneAndDelete
+	// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+	// 	console.log(result);
+
+	// });
+
+var obj = new ObjectID("597fbfb2981723123dd9f5ad");
+
+	// db.collection('Users').deleteMany({name: "Andrew"}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	 db.collection('Users').findOneAndDelete({_id: obj}).then((result) => {
+		console.log(JSON.stringify(result, undefined, 2));
+	 });
+
+	//db.close();
+});
