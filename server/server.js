0f0826223febbf450ac6864a8fa5f3cfd48bb28d
Luis Ramos <lr2471311@gmail.com>
2017-08-17 14:50:21 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose.js');
@@ -22,6 +23,8 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+
+
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		res.send({todos});
@@ -29,6 +32,25 @@ app.get('/todos', (req, res) => {
 		res.status(400).send(e);
 	})
 });
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	//find by id
+	Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+
+	}).catch((e) => {
+		res.status(400).send();
+	});
+}); 
 
 app.listen('3000', () => {
 	console.log('Started on port 3000');
