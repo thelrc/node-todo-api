ac3b8614cfdf2b359707488e02d59bbd4bee79fb
Luis Ramos <lr2471311@gmail.com>
2017-08-27 21:26:45 -0700

    Add DELETE /todos/:id

---
+++ playground/mongodb-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+// 	console.log(result);
+// });
+
+Todo.findByIdAndRemove('59a3912970c07ebbf66eda0c').then((todo) => {
+	console.log(todo);
+});
--- server/server.js
+++ server/server.js
@@ -51,7 +51,25 @@ app.get('/todos/:id', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send();
 	});
-}); 
+});
+
+app.delete('/todos/:id', (req,res) => {
+	//get the id
+	var id = req.params.id;
+	//vaildate the id not valid return a 404 error
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send(todo);
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
 
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`);
