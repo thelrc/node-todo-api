909bd3360620753a6fe7b2b8be17d931bc3758b9
Luis Ramos <lr2471311@gmail.com>
2017-08-28 17:43:25 -0700

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,4 +102,42 @@ describe('GET /todos/:id', () => {
 	});
 });
 
+describe('DELELTE /todos/:id', () => {
+	it('should remove a todo', (done) => {
+		var hexId = todos[1]._id.toHexString();
+
+		request(app)
+		.delete(`/todos/${hexId}`)
+		.expect(200)
+		.expect((res) => {
+			expect(res.body.todo._id).toBe(hexId);
+		})
+		.end((err, res) => {
+			if(err) {
+				return done(err);
+			}
+			//query the database findById
+			Todo.findById(hexId).then((todo) => {
+				expect(todo).toNotExist();
+				done();
+			}).catch((e) => done(e));
+
+		});
+	});
+	it('should return a 404 if todo not found', (done) => {
+			var hexId = new ObjectID().toHexString();	
+
+		request(app)
+		.delete(`/todos/${hexId}`)
+		.expect(404)
+		.end(done);
+	});
+	it('should return 404 if object if invalid', (done) => {
+		request(app)
+		.delete('/todos/123abc')
+		.expect(404)
+		.end(done);
+	});
+});
+
 
